05e7a5daa5f1f41d96923795d086d291892c95e7
SHIVXFOREX <111455999+SHIVXFOREX@users.noreply.github.com>
2023-01-16 14:35:36 +0530

    user input

---
+++ Indicators_Python/indicators.py
@@ -0,0 +1,52 @@
+import random
+import time
+from iqoptionapi.stable_api import IQ_Option
+Iq=IQ_Option("shivxforex@gmail.com","IDEAPAD300")
+Iq.connect()
+
+print(Iq.check_connect())
+balance_type="PRACTICE"
+
+ACTIVES="EURUSD"
+duration=1#minute 1 or 5
+default_Amt=10
+amount=10
+action="call"#put
+polling_time=3
+counter=1
+expirations_mode=1
+sell = []
+buy = []
+hold = []
+
+#Indicator Data
+ 
+while True :
+        sell = []
+        buy = []
+        hold = []
+        asset="EURUSD"
+        indicators = Iq.get_technical_indicators(asset)
+        for x in indicators:
+            if x['action'] == "sell" :
+                sell.append(x['action'])
+            elif x['action'] == "buy" :
+                buy.append(x['action'])
+            else :
+                hold.append(x['action'])
+                    
+        if len(buy) > len(sell) :
+            if len(buy) > len(hold) :
+                print("action - buy")
+            else :
+                print("action - hold")
+        else :
+            if len(sell) > len(hold) :
+                print("action - sELL")
+            else :
+                print("action - HOLD")
+
+
+
+
+
