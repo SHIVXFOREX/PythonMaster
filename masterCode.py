9e3764b34abd7393d1fa81d1a03e553c4365c3a2
SHIVXFOREX <111455999+SHIVXFOREX@users.noreply.github.com>
2023-01-17 14:32:18 +0530

    2:32

--- masterCode.py
+++ masterCode.py
@@ -6,7 +6,15 @@ from datetime import datetime
 import time
 from iqoptionapi.stable_api import IQ_Option
 import mysql.connector
+import ctypes 
+import easygui
 from mysql.connector import Error
+
+mydb = mysql.connector.connect(
+  host="156.67.222.169",
+  user="u733493607_pythondb",
+  password="python@3NGINE",
+)
 
 try:
     connection = mysql.connector.connect(host='156.67.222.169',
@@ -24,15 +32,20 @@ try:
 except Error as e:
     print("Error while connecting to MySQL", e)
 finally:
+    now = datetime.now()
+    seconds = now.strftime("%H:%M")
+    enter_time = easygui.enterbox("What, time is to start Robot 00:00 ?" , seconds)
+    ACTIVES = easygui.enterbox("Actives ? ?")
     if connection.is_connected():
         while True:
             time.sleep(1)
             now = datetime.now()
             dt_string = now.strftime("%d/%m/%Y %H:%M:%S")
-            seconds = now.strftime("%H:%M:%S")
-            print("min and sec =", seconds)
-            if seconds == "06:31:00":
-                print("time is 06:31:00")
+            seconds = now.strftime("%H:%M")
+            
+            print("HR and MIN =", seconds)
+            if seconds == enter_time:
+                print("time is ", enter_time)
                 error_password = """{"code":"invalid_credentials","message":"You entered the wrong credentials. Please check that the login/password is correct."}"""
                 Iq = IQ_Option("shivxforex@gmail.com", "IDEAPAD300")
                 check, reason = Iq.connect()
@@ -42,9 +55,9 @@ finally:
                     # if see this you can close network for test
                     balance_type = "PRACTICE"
                     print(Iq.change_balance(balance_type))
-                    Initial_Balance = int(Iq.get_balance())
+                    Initial_Balance = float(Iq.get_balance())
 
-                    ACTIVES = "EURUSD"
+                    
                     duration = 1  # minute 1 or 5
                     default_Amt = 1
                     amount = 1
@@ -77,13 +90,15 @@ finally:
                     # Master Loop
                     while True:
                         # MM
-                        file1 = open("EURUSD.txt", "a")
+                        file1 = open(ACTIVES + ".txt", "a")
                         if check_result(id) > 0:
+                            result = "WIN"
                             print("win")
                             lossCounter = 0
                             winCounter = winCounter + 1
                             action = action
                         else:
+                            result = "LOSS"
                             print("loss")
                             winCounter = 0
                             lossCounter = lossCounter + 1
@@ -113,14 +128,9 @@ finally:
                             if compundingAmt > 100:
                                 firstCompound = False
                                 print("Sleeping for 30 Min")
-                                sql = "UPDATE active SET acive = (%s) WHERE currency = (%s)";
-                                val = ("1",ACTIVES)
-                                cursor.execute(sql, val)
-
-                                connection.commit()
-
-                                print(cursor.rowcount, " Sleep record inserted.")
                                 time.sleep(1800)
+                        
+                        #Place Trade
 
                         _, id = (Iq.buy_digital_spot(
                             ACTIVES, amount, action, duration))
@@ -128,14 +138,13 @@ finally:
                         # Counter Controller
 
                         counter = counter + 1
-                        current_Bal = int(Iq.get_balance())
-                        sql = "UPDATE active SET acive = (%s) WHERE currency = (%s)";
-                        val = ("2",ACTIVES)
-                        cursor.execute(sql, val)
 
-                        connection.commit()
+                        #SQL UPDATE
+        
 
-                        print(cursor.rowcount, " active record inserted.")
+                        #Write Log..
+
+                    
                         print("-------------------------------------")
                         print("Counter :- " + str(counter))
                         print("Compounding Amt :- " + str(compundingAmt))
@@ -143,12 +152,15 @@ finally:
                         print("LossCounter :- " + str(lossCounter))
                         print("Balance :- ", Iq.get_balance())
                         print("Amount :- ", amount)
-                        print("Current Earning :- ", Initial_Balance - current_Bal)
-
+                        print("Current Earning :- ", current_Bal - Initial_Balance)
                         print("-------------------------------------")
+                        
+                        #Write File..
                         now = datetime.now()
+                        dt_string = now.strftime("%d/%m/%Y %H:%M:%S")
                         file1.write("T&D :- " + dt_string + " | ")
                         file1.write(ACTIVES + " | ")
+                        file1.write(result + " | ")
                         file1.write(action + " | ")
                         file1.write(str(amount))
                         file1.write(" | Counter :- " + str(counter) + " | ")
